80016eade5b0c853152ad9545e30fbeb7375d993
succ3ss0r <m4no4shi@protonmail.ch>
2017-10-12 21:35:43 +0200

    dodana klasa SpaceShip wraz z metodami, do klasy Star nalezy dopisac metode kolizji z dolna krawedzia /jezeli nastapi kolizja to usun obiekt star oraz stworz nowy star(rand()%800, 0)/

--- gwiazdy.cpp
+++ gwiazdy.cpp
@@ -1,7 +1,31 @@
 #include "SFML/Graphics.hpp"
 #include "SFML/Window.hpp"
 #include "cstdlib"
+#include "vector"
 
+class SpaceShip : public sf::Drawable
+{
+public:
+    SpaceShip(float t_X, float t_Y);
+    SpaceShip() = delete;
+    ~SpaceShip() = default;
+
+    void update();
+
+    sf::Vector2f getPosition();
+
+    float left();
+    float right();
+    float top();
+    float bottom();
+private:
+    void draw(sf::RenderTarget& target, sf::RenderStates state) const override;
+    sf::RectangleShape shape;
+    const float spaceShipWidth{ 30.0f };
+    const float spaceShipHeight{ 50.0f };
+    const float spaceShipVelocity{ 20.0f };
+    sf::Vector2f velocity { spaceShipVelocity, 0.f };
+};
 class Star : public sf::Drawable
 {
 public:
@@ -19,6 +43,7 @@ private:
     void draw(sf::RenderTarget& target, sf::RenderStates state) const override;
 
 };
+
 Star::Star(unsigned short t_X, unsigned short t_Y)
 {
     shape.setPosition(t_X, t_Y);
@@ -39,14 +64,66 @@ void Star::moveDown()
     this->velocity.y = starVelocity;
 }
 
+SpaceShip::SpaceShip(float t_X, float t_Y)
+{
+    shape.setPosition(t_X, t_Y);
+    shape.setSize(sf::Vector2f{this->spaceShipWidth, this->spaceShipHeight});
+    shape.setFillColor(sf::Color::Green);
+    shape.setOrigin(spaceShipWidth/2.f, spaceShipHeight/2.f);
+}
+void SpaceShip::draw(sf::RenderTarget& target, sf::RenderStates state) const
+{
+    target.draw(this->shape, state);
+}
+void SpaceShip::update()
+{
+    this->shape.move(this->velocity);
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && this->left() > 0)
+    {
+        velocity.x = -spaceShipVelocity;
+    }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && this->right() < 800)
+    {
+        velocity.x = spaceShipVelocity;
+    }else
+    {
+        velocity.x = 0;
+    }
+}
+float SpaceShip::left()
+{
+    return this->shape.getPosition().x - shape.getSize().x / 2.f;
+}
+float SpaceShip::right()
+{
+    return this->shape.getPosition().x + shape.getSize().x / 2.f;
+}
+float SpaceShip::top()
+{
+    return this->shape.getPosition().y - shape.getSize().y / 2.f;
+}
+float SpaceShip::bottom()
+{
+    return this->shape.getPosition().y + shape.getSize().y / 2.f;
+}
+sf::Vector2f SpaceShip::getPosition()
+{
+    return shape.getPosition();
+}
+
 int main()
 {
     sf::RenderWindow window( sf::VideoMode(800, 600), "Space Invaders by Serwicki & Siwon (BETA)" );
     window.setFramerateLimit(60);
     sf::Event event;
 
-    srand(time(NULL));
-    Star star(rand() % 800, 0);
+    SpaceShip battleShip(250, 550);
+
+    std::vector<Star> star;   //  Tworzenie obiektów gwiazd
+    for(int i = 0; i < 30; i++)
+    {
+        star.emplace_back(Star(rand()%800, rand()%600));
+    }
 
     while(true)
     {
@@ -58,9 +135,16 @@ int main()
             break;
         }
 
-        star.update();
+        battleShip.update();
 
-        window.draw(star);
+        for(int i = 0; i < 30; ++i)
+        {
+            star[i].update();
+
+            window.draw(star[i]);
+        }
+
+        window.draw(battleShip);
 
         window.display();
     }
