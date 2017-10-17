48bf0ac8f28bcd257ecbaedd5cea0464ac1b8291
succ3ss0r <m4no4shi@protonmail.ch>
2017-10-17 10:25:54 +0200

    utworzona klasa Enemy

--- gwiazdy.cpp
+++ gwiazdy.cpp
@@ -9,6 +9,31 @@
 #define SHIP_HEIGHT 50
 #define NUMBER_OF_STARS 50
 
+class Enemy : public sf::Drawable
+{
+public:
+    Enemy(float t_X, float t_Y, float t_Width, float t_Height);
+    Enemy() = delete;
+    ~Enemy() = default;
+
+    void update();
+    sf::Vector2f getPosition();
+
+    float left();
+    float right();
+    float top();
+    float bottom();
+
+    bool isDestroyed();
+    void destroy();
+    sf::Vector2f getSize();
+private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    sf::RectangleShape shape;
+    bool destroyed{ false };
+    const float enemyShipVelocity{ 1.0f };
+    sf::Vector2f velocity { enemyShipVelocity, 0.f };
+};
 class SpaceShip : public sf::Drawable
 {
 public:
@@ -126,14 +151,78 @@ float SpaceShip::bottom()
 {
     return this->shape.getPosition().y + shape.getSize().y / 2.f;
 }
+Enemy::Enemy(float t_X, float t_Y, float t_Width, float t_Height)
+{
+    shape.setPosition(t_X, t_Y);
+    shape.setSize(sf::Vector2f{t_Width, t_Height});
+    shape.setFillColor(sf::Color::Blue);
+    shape.setOrigin(t_Width/2.f, t_Height/2.f);
+}
+void Enemy::update()
+{
+    this->shape.move(this->velocity);
+
+//    if(this->left() < SCREEN_WIDTH)
+//    {
+//        velocity.x = -enemyShipVelocity;
+//    } else {
+//        velocity.x = enemyShipVelocity;
+//    }
+}
+void Enemy::draw(sf::RenderTarget& target, sf::RenderStates state) const
+{
+    target.draw(this->shape, state);
+}
+float Enemy::left()
+{
+    return this->shape.getPosition().x - shape.getSize().x / 2.f;
+}
+float Enemy::right()
+{
+    return this->shape.getPosition().x + shape.getSize().x / 2.f;
+}
+float Enemy::top()
+{
+    return this->shape.getPosition().y - shape.getSize().y / 2.f;
+}
+float Enemy::bottom()
+{
+    return this->shape.getPosition().y + shape.getSize().y / 2.f;
+}
+sf::Vector2f Enemy::getPosition()
+{
+    return shape.getPosition();
+}
+bool Enemy::isDestroyed()
+{
+    return this->destroyed;
+}
+void Enemy::destroy()
+{
+    this->destroyed = true;
+}
+sf::Vector2f Enemy::getSize()
+{
+    return shape.getSize();
+}
 
 int main()
 {
     sf::RenderWindow window( sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Space Invaders by Serwicki & Siwon (BETA)" );
     window.setFramerateLimit(60);
     sf::Event event;
+    int enemyX{ 10 }, enemyY{ 4 }, enemyWidth{ 40 }, enemyHeight{ 40 };
 
     SpaceShip battleShip(SCREEN_WIDTH/2-SHIP_WIDTH/2, SCREEN_HEIGHT-60);
+
+    std::vector<Enemy> enemies;
+    for(int i = 0; i < enemyY; i++)
+    {
+        for(int j = 0; j < enemyX; j++)
+        {
+            enemies.emplace_back((j + 3.5) * (enemyWidth + 10), (i + 1) * (enemyHeight + 10), enemyWidth, enemyHeight);
+        }
+    }
 
     std::vector<Star> star;   //  Tworzenie obiektów gwiazd
     for(int i = 0; i < NUMBER_OF_STARS; i++)
@@ -163,7 +252,15 @@ int main()
             window.draw(star[i]);
         }
 
+//        for(auto& enemy : enemies) if(collisionTest(enemy)) break;
+//
+//        auto iterator = remove_if(begin(enemies), end(enemies), [](Enemy& enemy){ return enemy.isDestroyed(); });
+//        enemies.erase(iterator, end(enemies));
+
         window.draw(battleShip);
+
+        for(auto& enemy : enemies)
+            window.draw(enemy);
 
         window.display();
     }
